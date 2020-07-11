5e53ad71c2bfd74f7223dd3633bb729a1a148f6f
Travis Deig <travisdeig@gmail.com>
2020-07-11 00:27:01 -0400

    Breadcrumbs

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 
 class CampsiteInfo extends Component {
@@ -21,7 +23,6 @@ class CampsiteInfo extends Component {
                 <Card>
                     <CardImg top src={campsite.image} alt={campsite.name} />
                     <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
                 </Card>
@@ -32,6 +33,16 @@ class CampsiteInfo extends Component {
         if (this.props.campsite) {
             return (
                 <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <Breadcrumb>
+                                <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{this.props.campsite.name}</BreadcrumbItem>
+                            </Breadcrumb>
+                            <h2>{this.props.campsite.name}</h2>
+                            <hr />
+                        </div>
+                    </div>
                     <div className="row">
                         {this.renderCampsite(this.props.campsite)}
                         {this.renderComments(this.props.comments)}
