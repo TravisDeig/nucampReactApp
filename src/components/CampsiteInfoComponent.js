e2943f1428c0e1e5b6b18239f32de877e6c49865
Travis Deig <travisdeig@gmail.com>
2020-07-18 13:30:33 -0400

    Comment Form

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,7 +1,78 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
+class CommentForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        }
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleSubmit.bind(this);
+    }
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+    handleSubmit(values) {
+        console.log("Current state is: " + JSON.stringify(values));
+        alert("Current state is: " + JSON.stringify(values));
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Button outline className="fa fa-pencil" onClick={this.toggleModal}>Submit Comment</Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <div className="form-group">
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select model=".rating" name="rating" id="rating" className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                                <Label htmlFor="author">Your Name</Label>
+                                <Control.text model=".author" name="author" id="author" placeholder="Your Name" className="form-control"
+                                    validators={{
+                                        required,
+                                        minLength: minLength(2),
+                                        maxLength: maxLength(15)
+                                    }}
+                                />
+                                <Errors
+                                    className="text-danger"
+                                    model=".author"
+                                    show="touched"
+                                    component="div"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be at least 2 characters',
+                                        maxLength: 'Must be 15 characters or less'
+                                    }}
+                                />
+                                <Label htmlFor="text">Comment</Label>
+                                <Control.textarea model=".text" name="text" id="text" rows="6" className="form-control" />
+                            </div>
+                            <Button type="submit" value="submit" color="primary">Submit</Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </div>
+        );
+    }
+}
 
 
 class CampsiteInfo extends Component {
@@ -11,6 +82,7 @@ class CampsiteInfo extends Component {
                 <div className="col-md-5 m-1">
                     <h4>Comments</h4>
                     {comments.map(comment => <div className="mb-3" key={comments.id}>{comment.text}<br /> --{comment.author} ,{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</div>)}
+                    <CommentForm />
                 </div>
             );
         }
@@ -29,6 +101,7 @@ class CampsiteInfo extends Component {
             </div>
         );
     }
+
     render() {
         if (this.props.campsite) {
             return (
@@ -50,6 +123,8 @@ class CampsiteInfo extends Component {
                 </div>
             );
         }
+
+
         return <div />;
     }
 }
